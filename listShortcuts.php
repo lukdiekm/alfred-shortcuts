be5215133f97215345710acf4ff3ea707b974adf
Lukas <mail@lukas-diekmann.de>
2021-07-27 21:19:12 +0200

    activate filter when typing

--- listShortcuts.php
+++ listShortcuts.php
@@ -1,12 +1,12 @@
 <?php
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><items>";
 exec('shortcuts list', $data);
-    foreach ($data as $shortcut) {
-            echo "<item uid=\"'{$shortcut}\" valid=\"YES\" autocomplete=\"{$shortcut}\">";
-            echo "<title>{$shortcut}</title>";
-            echo "<subtitle>{$shortcut}</subtitle>";
-            echo "<icon>sc-icon.png</icon>";
-            echo "<arg>{$shortcut}</arg>";
-            echo "</item>";   
+foreach ($data as $shortcut) {
+    echo "<item uid=\"'{$shortcut}\" valid=\"YES\" autocomplete=\"{$shortcut}\">";
+    echo "<title>{$shortcut}</title>";
+    echo "<subtitle>{$shortcut}</subtitle>";
+    echo "<icon>sc-icon.png</icon>";
+    echo "<arg>{$shortcut}</arg>";
+    echo "</item>";
 }
 echo "</items>";
