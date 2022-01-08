6123c934fa76d3c52fe0c206aa227fc9c79959b3
Lukas <mail@lukas-diekmann.de>
2022-01-08 22:47:20 +0100

    Revert "color icons per folder"
    This reverts commit cee482734944b483479f1e95b365dc496d4ee83d.

--- listShortcuts.sh
+++ listShortcuts.sh
@@ -1,53 +1,29 @@
-#!/bin/bash
-
-getIcon() {
+getShortcuts () {
+if [[ ! -z "$folders" ]]; then
     IFS=','
-    read -raarr <<<"$colors"
-    for color in "${arr[@]}"; do
-        IFS='='
-        read -ratmp <<<"$color"
-        if [[ "${tmp[0]}" == "$1" ]]; then
-            echo "${tmp[1]}.png"
-            return 0
-        fi
+    read -a arr <<<"$folders"
+    for folder in "${arr[@]}"; do
+        shortcuts list --folder-name "$folder"
     done
-    echo "icon.png"
     IFS=''
-}
-
-printShortcut() {
-    printf "<item uid=\"%s\" valid=\"YES\" autocomplete=\"%s\">" $1 $2
-    printf "<title>%s</title>" $1
-    printf "<subtitle>%s</subtitle>" $2
-    printf "<icon>icons/%s</icon>" $3
-    printf "<arg>%s</arg>" $1
-    printf "</item>"
-}
-
-printFolder() {
-    ic=$(getIcon "$folder")
-    shortcuts list --folder-name "$folder" |
-        while read shortcut; do
-            printShortcut $shortcut $folder $ic
-        done
+elif [[ ! -z "$folder" ]]; then
+    shortcuts list --folder-name "$folder"
+else
+    shortcuts list
+fi
 }
 
 printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?><items>"
 IFS=''
 
-if [[ ! -z "$folders" ]]; then
-    IFS=','
-    read -ra arr <<<"$folders"
-    for folder in "${arr[@]}"; do
-        printFolder "$folder"
-    done
-    IFS=''
-elif [[ ! -z "$folder" ]]; then
-    printFolder "$folder"
-else
-    shortcuts list --folders |
-        while read folder; do
-            printFolder "$folder"
-        done
-fi
+getShortcuts |
+while read shortcut
+do
+    printf "<item uid=\"%s\" valid=\"YES\" autocomplete=\"%s\">" $shortcut $shortcut
+    printf "<title>%s</title>" $shortcut
+    printf "<subtitle>%s</subtitle>" $shortcut
+    printf "<icon>sc-icon.png</icon>"
+    printf "<arg>%s</arg>" $shortcut
+    printf "</item>"
+done
 echo "</items>"
